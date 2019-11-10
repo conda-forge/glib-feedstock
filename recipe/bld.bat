930adf49e541f206da19e8f29090d4269a19d325
Peter Williams <peter@newton.cx>
2019-11-10 17:03:53 -0500

    recipe/bld.bat: flail a bit on Windows
    The current issue is that linking libgio fails because the command to link to libz
    isn't getting passed to the linker correctly.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,7 +13,7 @@ cd forgebuild
 
 @REM Find libffi with pkg-config
 FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
-set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig
+set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig;%LIBRARY_PREFIX_M%/share/pkgconfig
 
 @REM Avoid a Meson issue - https://github.com/mesonbuild/meson/issues/4827
 set "PYTHONLEGACYWINDOWSSTDIO=1"
@@ -21,6 +21,12 @@ set "PYTHONIOENCODING=UTF-8"
 
 %BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Diconv=gnu -Dselinux=false -Dxattr=false -Dlibmount=false ..
 if errorlevel 1 exit 1
+
+echo BUILD DOT NINJA
+type build.ninja
+echo MESON LOG
+type meson-logs/meson-log.txt
+echo EOF
 
 ninja -v
 if errorlevel 1 exit 1

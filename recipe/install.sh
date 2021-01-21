f64e5bee211940a373c6eaf7bfd4542f26c27a08
Uwe L. Korn <uwelk@xhochy.com>
2021-01-21 22:27:23 +0100

    Add a glib-tools package

--- recipe/install.sh
+++ recipe/install.sh
@@ -19,13 +19,20 @@ if [[ "$PKG_NAME" == glib ]]; then
         cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
      done
 else
-    mkdir .keep
-    # We ship glib-compile-* and gio-* binaries as part of the libglib packages
-    # as they are native binaries and also come along inside of libglib
-    # in other distributions.
-    mv $PREFIX/bin/glib-compile* .keep
+    if [[ "$PKG_NAME" == glib-tools ]]; then
+        mkdir .keep
+        # We ship glib-compile-* and gio-* binaries as part of the libglib packages
+        # as they are native binaries and also come along inside of libglib
+        # in other distributions.
+        mv $PREFIX/bin/glib-compile* .keep
+    else
+        rm $PREFIX/bin/gio*
+        rm -r $PREFIX/share/bash-completion/completions/gio
+    fi
     rm $PREFIX/bin/gdbus* $PREFIX/bin/glib* $PREFIX/bin/gobject* $PREFIX/bin/gresource $PREFIX/bin/gsettings $PREFIX/bin/gtester*
-    mv .keep/glib-compile* $PREFIX/bin
+    if [[ "$PKG_NAME" == glib-tools ]]; then
+        mv .keep/glib-compile* $PREFIX/bin
+    fi
     rm -r $PREFIX/include/gio-* $PREFIX/include/glib-*
     rm -r $PREFIX/lib/pkgconfig/{gio*,glib*,gmodule*,gobject*,gthread*}
     rm -r $PREFIX/share/aclocal/{glib*,gsettings*}

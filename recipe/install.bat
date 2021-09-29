82043484a0160eaa48f6cf172a9cda9f587fabfc
Ryan Volz <ryan.volz@gmail.com>
2021-09-29 11:32:36 -0400

    Move a few more binaries to glib-tools.
    These tools, notably gdbus, are needed by e.g. gtk3 at runtime, but
    other packages compiling against glib don't necessarily need them in
    libglib.
    
    This also shifts some extra things that are only needed at build
    time from libglib to glib (e.g. gettext, a stray gapplication binary).
    Finally, glib-compile-resource moves from glib-tools to glib since it is
    only needed at build time.
    
    This organization roughly matches Debian:
    libglib == libglib2.0 and libglib2.0-data (plus pkgconfig from -dev)
    glib-tools == libglib2.0-bin
    glib == libglib2.0-dev and libglib2.0-dev-bin

--- recipe/install.bat
+++ recipe/install.bat
@@ -4,59 +4,69 @@ if errorlevel 1 exit 1
 
 if NOT [%PKG_NAME%] == [glib] (
   if [%PKG_NAME%] == [libglib] (
+      del %LIBRARY_PREFIX%\bin\gdbus.exe
+      if errorlevel 1 exit 1
       del %LIBRARY_PREFIX%\bin\gio-querymodules.exe
       if errorlevel 1 exit 1
       del %LIBRARY_PREFIX%\bin\gio.exe
       if errorlevel 1 exit 1
-      del %LIBRARY_PREFIX%\bin\glib-compile-resources.exe
-      if errorlevel 1 exit 1
       del %LIBRARY_PREFIX%\bin\glib-compile-schemas.exe
       if errorlevel 1 exit 1
+      del %LIBRARY_PREFIX%\bin\gresource.exe
+      if errorlevel 1 exit 1
+      del %LIBRARY_PREFIX%\bin\gsettings.exe
+      if errorlevel 1 exit 1
+      del %LIBRARY_PREFIX%\share\bash-completion\completions\gapplication
+      if errorlevel 1 exit 1
+      del %LIBRARY_PREFIX%\share\bash-completion\completions\gdbus
+      if errorlevel 1 exit 1
       del %LIBRARY_PREFIX%\share\bash-completion\completions\gio
+      if errorlevel 1 exit 1
+      del %LIBRARY_PREFIX%\share\bash-completion\completions\gresource
+      if errorlevel 1 exit 1
+      del %LIBRARY_PREFIX%\share\bash-completion\completions\gsettings
       if errorlevel 1 exit 1
   )
   del %LIBRARY_PREFIX%\bin\gdbus-codegen
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\bin\gdbus.exe
+  del %LIBRARY_PREFIX%\bin\glib-compile-resources.exe
   if errorlevel 1 exit 1
   del %LIBRARY_PREFIX%\bin\glib-genmarshal
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\bin\glib-gettextsize
+  del %LIBRARY_PREFIX%\bin\glib-gettextize
   if errorlevel 1 exit 1
   del %LIBRARY_PREFIX%\bin\glib-mkenums
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\bin\gobject-query
-  if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\bin\gresource.exe
-  if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\bin\gsettings.exe
+  del %LIBRARY_PREFIX%\bin\gobject-query.exe
   if errorlevel 1 exit 1
   del %LIBRARY_PREFIX%\bin\gspawn*
   if errorlevel 1 exit 1
   del %LIBRARY_PREFIX%\bin\gtester*
   if errorlevel 1 exit 1
 
-  del %LIBRARY_PREFIX%\include\glib*
+  del %LIBRARY_PREFIX%\include\gio-win32-2.0
+  if errorlevel 1 exit 1
+  del %LIBRARY_PREFIX%\include\glib-2.0
   if errorlevel 1 exit 1
 
-  del %LIBRARY_PREFIX%\lib\pkgconfig\gio*
+  del %LIBRARY_PREFIX%\lib\pkgconfig\gio-*
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\lib\pkgconfig\glib*
+  del %LIBRARY_PREFIX%\lib\pkgconfig\glib-*
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\lib\pkgconfig\gmodule*
+  del %LIBRARY_PREFIX%\lib\pkgconfig\gmodule-*
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\lib\pkgconfig\gobject*
+  del %LIBRARY_PREFIX%\lib\pkgconfig\gobject-*
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\lib\pkgconfig\gthread*
+  del %LIBRARY_PREFIX%\lib\pkgconfig\gthread-*
   if errorlevel 1 exit 1
 
-  del %LIBRARY_PREFIX%\share\bash-completion\completions\gapplication
+  del %LIBRARY_PREFIX%\share\aclocal\glib-*
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\share\bash-completion\completions\gdbus
+  del %LIBRARY_PREFIX%\share\aclocal\gsettings.m4
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\share\bash-completion\completions\gresource
+  del %LIBRARY_PREFIX%\share\gettext\its
   if errorlevel 1 exit 1
-  del %LIBRARY_PREFIX%\share\bash-completion\completions\gsettings
+  del %LIBRARY_PREFIX%\share\glib-2.0
   if errorlevel 1 exit 1
 )
 
--- recipe/install.sh
+++ recipe/install.sh
@@ -21,21 +21,26 @@ if [[ "$PKG_NAME" == glib ]]; then
 else
     if [[ "$PKG_NAME" == glib-tools ]]; then
         mkdir .keep
-        # We ship glib-compile-* and gio-* binaries as part of the libglib packages
-        # as they are native binaries and also come along inside of libglib
-        # in other distributions.
-        mv $PREFIX/bin/glib-compile* .keep
+        # We ship these binaries as part of the glib-tools package because
+        # they can be depended on separately by other packages, e.g. gtk
+        # (equivalent to Debian's libglib2.0-bin)
+        mv $PREFIX/bin/gdbus .keep
+        mv $PREFIX/bin/glib-compile-schemas .keep
     else
+        rm -f $PREFIX/bin/gapplication
         rm $PREFIX/bin/gio*
-        rm -r $PREFIX/share/bash-completion/completions/gio
+        rm $PREFIX/bin/gresource
+        rm $PREFIX/bin/gsettings
+        rm $PREFIX/share/bash-completion/completions/{gapplication,gdbus,gio,gresource,gsettings}
     fi
-    rm $PREFIX/bin/gdbus* $PREFIX/bin/glib* $PREFIX/bin/gobject* $PREFIX/bin/gresource $PREFIX/bin/gsettings $PREFIX/bin/gtester*
+    rm $PREFIX/bin/{gdbus*,glib-*,gobject*,gtester*}
     if [[ "$PKG_NAME" == glib-tools ]]; then
-        mv .keep/glib-compile* $PREFIX/bin
+        mv .keep/* $PREFIX/bin
     fi
     rm -r $PREFIX/include/gio-* $PREFIX/include/glib-*
-    rm -r $PREFIX/share/aclocal/{glib*,gsettings*}
-    rm -r $PREFIX/share/bash-completion/completions/{gapplication,gdbus,gresource,gsettings}
-    rm -r $PREFIX/share/glib-*
+    rm -r $PREFIX/lib/glib-*
     rm -r $PREFIX/lib/lib{gmodule,glib,gobject,gthread,gio}-2.0${SHLIB_EXT}
+    rm -r $PREFIX/share/aclocal/{glib-*,gsettings*}
+    rm -r $PREFIX/share/gettext/its
+    rm -r $PREFIX/share/glib-*
 fi

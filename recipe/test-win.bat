7f28ae4716f0e7e6a1c20f7d6e70f24b911f903f
Uwe L. Korn <xhochy@users.noreply.github.com>
2024-03-16 12:38:31 +0100

    test-win.bat aktualisieren

--- recipe/test-win.bat
+++ recipe/test-win.bat
@@ -1,7 +1,7 @@
 @echo on
 
 for /f "delims=" %%a in ('pkg-config --libs --msvc-syntax glib-2.0') do (
-    %CC% /link "%%a" /I "%LIBRARY_PREFIX%\include\glib-2.0" /I %LIBRARY_PREFIX%\lib\glib-2.0\include /Fe:output.exe test.c
+    %CC% /I "%LIBRARY_PREFIX%\include\glib-2.0" /I %LIBRARY_PREFIX%\lib\glib-2.0\include /Fe:output.exe test.c /link "%%a"
     if errorlevel 1 exit 1
 )
 

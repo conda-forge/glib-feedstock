45eb1447b2fcd80cfb5180c33e5d06329d3d39df
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-18 12:08:36 +0100

    Don't delete intl.lib

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -22,9 +22,6 @@ set "PYTHONIOENCODING=UTF-8"
 @REM See hardcoded-paths.patch
 set "CPPFLAGS=%CPPFLAGS% -D^"%LIBRARY_PREFIX_M%^""
 
-del %LIBRARY_PREFIX%\lib\intl.lib
-if errorlevel 1 exit 1
-
 meson setup --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Dselinux=disabled -Dxattr=false -Dlibmount=disabled -Dnls=enabled ..
 if errorlevel 1 exit 1
 

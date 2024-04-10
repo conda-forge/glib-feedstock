9f300b901fefc22aaf4c42a2ba1063facd82162e
Uwe Korn <uwelk@xhochy.com>
2024-04-10 19:14:18 +0000

    Add GIR_PREFIX to pkg-config

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -21,7 +21,8 @@ cd forgebuild
 
 @REM Find libffi with pkg-config
 FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
-set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig;%LIBRARY_PREFIX_M%/share/pkgconfig
+FOR /F "delims=" %%i IN ('cygpath.exe -m "%GIR_PREFIX%"') DO set "GIR_PREFIX_M=%%i"
+set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig;%LIBRARY_PREFIX_M%/share/pkgconfig;%GIR_PREFIX_M%/Library/lib/pkgconfig
 
 @REM Avoid a Meson issue - https://github.com/mesonbuild/meson/issues/4827
 set "PYTHONLEGACYWINDOWSSTDIO=1"

dd855bc64f992a9de96faaa66ffdae8fae773f55
Uwe Korn <uwelk@xhochy.com>
2024-04-11 08:29:29 +0000

    Verbose g-ir-scanner

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,7 +13,7 @@ set "GIR_PREFIX=%cd%\g-ir-prefix"
 call conda create -p %GIR_PREFIX% -y g-ir-build-tools gobject-introspection glib
 if errorlevel 1 exit 1
 
-echo @echo off > %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
+echo @echo on > %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
 echo call %GIR_PREFIX%\bin\g-ir-scanner "%%*" >> %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
 
 mkdir forgebuild

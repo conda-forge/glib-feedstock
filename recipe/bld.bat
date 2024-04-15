2c30cb51952f6e2c9dff813841adff4ad63f0b37
Uwe Korn <uwelk@xhochy.com>
2024-04-15 09:24:04 +0000

    Expand path globally

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,6 +13,8 @@ set "GIR_PREFIX=%cd%\g-ir-prefix"
 call conda create -p %GIR_PREFIX% -y g-ir-build-tools gobject-introspection glib
 if errorlevel 1 exit 1
 
+set PATH="%PATH%;%GIR_PREFIX%\Library;%GIR_PREFIX%\Library\bin"
+
 echo @echo on > %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
 echo set PATH="%%PATH%%;%GIR_PREFIX%\Library;%GIR_PREFIX%\Library\bin" >> %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
 echo call %GIR_PREFIX%\bin\g-ir-scanner %%* --verbose >> %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat

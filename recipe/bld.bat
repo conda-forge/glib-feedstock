b338c2504a3bb5eb3e1b4fe449db89b1ceabe1f0
Uwe Korn <uwelk@xhochy.com>
2024-04-15 08:39:53 +0000

    Different type of verbosity

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,7 +14,7 @@ call conda create -p %GIR_PREFIX% -y g-ir-build-tools gobject-introspection glib
 if errorlevel 1 exit 1
 
 echo @echo on > %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
-echo call %GIR_PREFIX%\bin\g-ir-scanner "%%* --verbose" >> %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
+echo call %GIR_PREFIX%\bin\g-ir-scanner %%* --verbose >> %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
 type %BUILD_PREFIX%\Library\bin\g-ir-scanner.bat
 
 mkdir forgebuild
@@ -38,7 +38,7 @@ if errorlevel 1 exit 1
 type meson-logs\meson-log.txt
 if errorlevel 1 exit 1
 
-ninja -v
+ninja -v -j1
 if errorlevel 1 exit 1
 
 @REM Lots of tests fail right now

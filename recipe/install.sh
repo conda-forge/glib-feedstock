ddb875fcb14c4d090da7e4c1d8d37c8d7fe67fb7
Mark Harfouche <mark.harfouche@gmail.com>
2024-10-03 12:58:07 -0400

    self review

--- recipe/install.sh
+++ recipe/install.sh
@@ -14,11 +14,6 @@ rm -rf $PREFIX/share/gdb
 
 if [[ "${PKG_NAME}" == "glib" ]]; then
 
-    echo ${PREFIX}
-    echo BEFORE
-    head -n 1 ${PREFIX}/bin/glib-mkenums
-    head -n 1 ${PREFIX}/bin/glib-genmarshal
-    head -n 1 ${PREFIX}/bin/gtester-report
     # unsured about why the build prefix of the split package is appearing
     # in there, but we must remove it
     sed -i.bak "1s|^#!.*|#!${PREFIX}/bin/python|" "${PREFIX}/bin/glib-mkenums"
@@ -29,10 +24,7 @@ if [[ "${PKG_NAME}" == "glib" ]]; then
     rm ${PREFIX}/bin/glib-genmarshal.bak
     rm ${PREFIX}/bin/gtester-report.bak
 
-    echo AFTER
-    head -n 1 ${PREFIX}/bin/glib-mkenums
-    head -n 1 ${PREFIX}/bin/glib-genmarshal
-    head -n 1 ${PREFIX}/bin/gtester-report
+    # The remainder of the script is only for other packages. exit here with success.
     exit 0
 elif [[ "$PKG_NAME" == glib-tools ]]; then
     mkdir .keep

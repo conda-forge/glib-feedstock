b290b5c99295ed0612dba0b19979d5e6c7a57d49
Peter Williams <peter@newton.cx>
2026-02-11 22:06:42 -0800

    Some tidying of the Windows build scripting

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,8 @@
 @ECHO ON
 
-@REM I cannot for the life of me figure out how Cygwin/MSYS2 figures out its
-@REM root directory, which it uses to find the /etc/fstab which *sometimes*
-@REM affects the choice of the cygdrive prefix. But, regardless of *why*,
-@REM I find that we need this to work:
-mkdir %BUILD_PREFIX%\Library\etc
-echo none / cygdrive binary,user 0 0 >%BUILD_PREFIX%\Library\etc\fstab
-echo none /tmp usertemp binary,posix=0 0 0 >>%BUILD_PREFIX%\Library\etc\fstab
-
 set "GIR_PREFIX=%cd%\g-ir-prefix"
 
-call conda create -p %GIR_PREFIX% -y g-ir-build-tools gobject-introspection glib "setuptools<71"
+call conda create -p %GIR_PREFIX% -y g-ir-build-tools gobject-introspection glib
 if errorlevel 1 exit 1
 
 set "PATH=%PATH%;%GIR_PREFIX%\Library;%GIR_PREFIX%\Library\bin"
